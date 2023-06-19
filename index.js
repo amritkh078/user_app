49a405386a38aef42c09478c63c7052704504ae1
amritkh078 <amritkhatiwada078@gmail.com>
2023-06-19 22:30:48 +0545

    routes imported

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const client = require('./config/dbcon.js');
+const router = require('./routes/appRoutes.js');
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use( router);
 
 client.connect(
     function(err) {
@@ -15,4 +19,6 @@ client.connect(
     }
 );
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
