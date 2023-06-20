74ae3b794966a1a6843d9db79025bcf7bc2fcabd
amritkh078 <amritkhatiwada078@gmail.com>
2023-06-20 06:27:53 +0545

    routes updated with validation

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const appController = require('../controllers/appController');
+const { validateRecord } = require('../middleware/validateRecord');
 
 // Create a new record
-router.post('/records', appController.createRecord);
+router.post('/records',validateRecord, appController.createRecord);
 
 // Get all records
-router.get('/records', appController.getAllRecords);
+router.get('/records',validateRecord, appController.getAllRecords);
 
 // Get a record by ID
-router.get('/records/:id', appController.getRecordById);
+router.get('/records/:id',validateRecord, appController.getRecordById);
 
 // Update a record
-router.put('/records/:id', appController.updateRecord);
+router.put('/records/:id',validateRecord, appController.updateRecord);
 
 // Delete a record
-router.delete('/records/:id', appController.deleteRecord);
+router.delete('/records/:id',validateRecord, appController.deleteRecord);
 
 module.exports = router;
