6b370375eecad8c4978b269cb19d98cf0bda25e2
amritkh078 <amritkhatiwada078@gmail.com>
2023-06-20 06:55:11 +0545

    phone validation

--- middleware/validateRecord.js
+++ middleware/validateRecord.js
@@ -4,7 +4,7 @@ const client = require('../config/dbcon.js');
 const recordSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
-    phone: Joi.string().max(10).required(),
+    phone: Joi.number().integer().max(9999999999).required(),
 });
 
 module.exports = {
